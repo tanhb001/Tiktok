b685b04a0e76fc0c5a295922c269f5e679001a3f
Tan Dang <tanhb001@gmail.com>
2022-09-15 14:50:10 +0700

    update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,8 +12,20 @@ function Home() {
             <Content
                 name="nguyen_thanhtuyen"
                 nickname="Nguyen Thanh Tuyen"
-                avatar="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/e475fbd1477dc5e19b66be0cf273cf64~c5_100x100.jpeg?x-expires=1663214400&x-signature=OzAx2UYCwbnSlCph8ROtLA1X15s%3D"
-                videoUrl="https://v16-webapp.tiktok.com/a604541346bf9a6cc91a2cf3a8b938c9/632059a7/video/tos/useast2a/tos-useast2a-pve-0037-aiso/5273fbddb5804cbcbe1bf4809ed1f6bc/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=3066&bt=1533&cs=0&ds=3&ft=eXd.6Hk_Myq8ZTHjphe2NLWQml7Gb&mime_type=video_mp4&qs=0&rc=OzVlOzs8NTpnaDs8ZWk1ZkBpamlxNWk6Zjk0ZTMzZjgzM0BjNGI2NTRgXl8xYV5eNjRgYSNyMGBtcjRncF5gLS1kL2Nzcw%3D%3D&l=202209130421160102451411370B256A73&btag=80000"
+                avatar="https://genzrelax.com/wp-content/uploads/2022/03/hinh-gai-xinh.jpg"
+                videoUrl="https://v16-webapp.tiktok.com/5d2e6988b559c0ac0a5dd163b5958e52/6323016a/video/tos/maliva/tos-maliva-ve-0068c799-us/dfeedf42bc89499aa5fd63ac0fd30609/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=2250&bt=1125&cs=0&ds=3&ft=eXd.6Hk_Myq8Zbpephe2Ngyhml7Gb&mime_type=video_mp4&qs=0&rc=Z2VnZjwzaWk2OjtnOzw4N0BpM3U4NTc6ZmhuZjMzZzczNEAwYzAxYl4tNmAxXzY0MGAtYSMwbTZicjRvNmNgLS1kMS9zcw%3D%3D&l=2022091504412901024504007605037565&btag=80000"
+            />
+            <Content 
+                name="user"
+                nickname="user"
+                avatar="https://salt.tikicdn.com/cache/256x256/ts/avatar/6d/fc/02/c5a4aa4f703a3f96daefdb072d39b9dd.jpg"
+                videoUrl="https://v16-webapp.tiktok.com/898b78631af59ecd0a72501b9dca86be/63230160/video/tos/useast2a/tos-useast2a-pve-0037-aiso/adee390ede5f48b8b015542dc4d5f4fe/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=5024&bt=2512&cs=0&ds=3&ft=eXd.6Hk_Myq8Zbpephe2Ngyhml7Gb&mime_type=video_mp4&qs=0&rc=ZDk2ZWg3NDNlZjNlODU3OEBpanF0dTY6ZnZkZjMzZjgzM0BgLzMvNS5fX14xMF8yMWA2YSM0MmExcjRfc19gLS1kL2Nzcw%3D%3D&l=2022091504412901024504007605037565&btag=80000"
+            />
+            <Content 
+                name="user1"
+                nickname="user1"
+                avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnpSV72tWT3M_9ssJsk3Aj57rXuw34En-lXg&usqp=CAU"
+                videoUrl="https://v16-webapp.tiktok.com/6850c37a263d876d446b54264fd60f45/63230175/video/tos/alisg/tos-alisg-pve-0037/d5de94eeaa364b5eb3187c1a76520533/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=2708&bt=1354&cs=0&ds=3&ft=eXd.6Hk_Myq8Zbpephe2Ngyhml7Gb&mime_type=video_mp4&qs=0&rc=NzRoPGk7aDtlOzU7PGQ8NUBpajVrajk6ZjppZTMzODgzNEAvNGAwX15eNS0xLzA2LzMwYSNuXi1fcjRfMzBgLS1kLy1zcw%3D%3D&l=2022091504412901024504007605037565&btag=80000"
             />
         </div>
     );
